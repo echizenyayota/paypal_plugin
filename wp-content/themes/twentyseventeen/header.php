2d2106f06a923a857ee541d8508368fde48e9380
echizenyayota <virtualmisso30@gmail.com>
2018-04-25 15:38:38 +0900

    write_directly https://developer.paypal.com/demo/checkout/#/pattern/client

--- wp-content/themes/twentyseventeen/header.php
+++ wp-content/themes/twentyseventeen/header.php
@@ -18,7 +18,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-
+<script src="https://www.paypalobjects.com/api/checkout.js"></script>
 <?php wp_head(); ?>
 </head>
 
