73f5b77af286b66f1162444c6f4f1d1ef742916b
echizenyayota <virtualmisso30@gmail.com>
2018-05-02 14:16:19 +0900

    jsフォルダの作成とexpresscheckout.jsの読み込み

--- wp-content/plugins/paypal_expresscheckout/express.php
+++ wp-content/plugins/paypal_expresscheckout/express.php
@@ -20,3 +20,12 @@ function hook_paypalexpress() {
 	$output = '<script src="https://www.paypalobjects.com/api/checkout.js"></script>';
 	echo $output;
 }
+
+// expresscheckout.jsの読み込み
+function plugin_paypalexpress_scripts() {
+
+  wp_enqueue_style( 'plugin_paypalexpress_scripts', plugins_url( '/js/expresscheckout.js', __FILE__ ), array(), date('U') );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'plugin_paypalexpress_scripts' );
