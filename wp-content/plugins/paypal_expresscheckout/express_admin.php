6515c56be4fb13fc895326aead757e17685e3144
echizenyayota <virtualmisso30@gmail.com>
2018-05-09 18:32:44 +0900

    formタグのPOSTメソッド先を指定

--- wp-content/plugins/paypal_expresscheckout/express_admin.php
+++ wp-content/plugins/paypal_expresscheckout/express_admin.php
@@ -9,7 +9,7 @@ add_action('admin_menu', 'paypalexpresscheckout_add_admin_menu');
 function paypalexpresscheckout_admin_menu() {
 	echo <<<EOD
 	<h2>PayPal Express Checkoutの設定画面</h2>
-		<form action="" method="">
+		<form action="express.php" method="post">
 			color:
 			<input type="radio" name="color" value="gold" checked> gold
       <input type="radio" name="color" value="blue"> blue
