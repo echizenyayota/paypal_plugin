5f0b512c5b38fad0003f5fdc22050953779a35e0
echizenyayota <virtualmisso30@gmail.com>
2018-05-09 17:18:15 +0900

    管理画面の表示と管理画面の設定（ボタンの色）

--- wp-content/plugins/paypal_expresscheckout/express.php
+++ wp-content/plugins/paypal_expresscheckout/express.php
@@ -18,7 +18,7 @@ function paypal_scripts() {
 	wp_enqueue_script( 'paypal-checkout', 'https://www.paypalobjects.com/api/checkout.js' );
 	wp_enqueue_script( 'paypal-expresscheckout', plugin_dir_url( __FILE__ ) . '/js/expresscheckout.js', array( 'paypal-checkout' ) );
 	wp_localize_script( 'paypal-expresscheckout', 'paypal_expresscheckout_param', array(
-			'color' => 'gold',
+			'color' => 'blue', // gold, blue, silver, black
 	) );
 }
 add_action( 'wp_enqueue_scripts', 'paypal_scripts' );
@@ -29,3 +29,23 @@ function paypaldiv_func(){
   return $paypaldiv;
 }
 add_shortcode( 'paypaldiv', 'paypaldiv_func' );
+
+// 管理画面の表示
+function paypalexpresscheckout_add_admin_menu(){
+    add_submenu_page('plugins.php','PayPal Express Checkoutの設定','PayPal Express Checkoutの設定', 'administrator', __FILE__, 'paypalexpresscheckout_admin_menu');
+}
+add_action('admin_menu', 'paypalexpresscheckout_add_admin_menu');
+
+// 管理画面の設定
+function paypalexpresscheckout_admin_menu() {
+	echo <<<EOD
+	<h2>PayPal Express Checkoutの設定画面</h2>
+		<form action="" method="">
+			style_color:
+			<input type="radio" name="color" value="gold" checked> gold
+      <input type="radio" name="color" value="blue"> blue
+			<input type="radio" name="color" value="silver"> silver
+			<input type="radio" name="color" value="black"> black
+		<form>
+EOD;
+}
