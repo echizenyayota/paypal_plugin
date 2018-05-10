a1aec4ac18046b60b169399495cf6e3f1ff9a0b7
echizenyayota <virtualmisso30@gmail.com>
2018-05-10 12:27:24 +0900

    プラグイン本体ファイルと管理画面ファイルの分離

--- wp-content/plugins/paypal_expresscheckout/express.php
+++ wp-content/plugins/paypal_expresscheckout/express.php
@@ -30,23 +30,25 @@ function paypaldiv_func(){
 }
 add_shortcode( 'paypaldiv', 'paypaldiv_func' );
 
-// 管理画面の表示
-function paypalexpresscheckout_add_admin_menu(){
-    add_submenu_page('plugins.php','PayPal Express Checkoutの設定','PayPal Express Checkoutの設定', 'administrator', __FILE__, 'paypalexpresscheckout_admin_menu');
-}
-add_action('admin_menu', 'paypalexpresscheckout_add_admin_menu');
+require_once(plugin_dir_url( __FILE__ ) . 'express_admin.php');
 
-// 管理画面の設定
-function paypalexpresscheckout_admin_menu() {
-	echo <<<EOD
-	<h2>PayPal Express Checkoutの設定画面</h2>
-		<form action="" method="">
-			color:
-			<input type="radio" name="color" value="gold" checked> gold
-      <input type="radio" name="color" value="blue"> blue
-			<input type="radio" name="color" value="silver"> silver
-			<input type="radio" name="color" value="black"> black
-			<input type="submit" value="保存">
-		<form>
-EOD;
-}
+// 管理画面の表示
+// function paypalexpresscheckout_add_admin_menu(){
+//     add_submenu_page('plugins.php','PayPal Express Checkoutの設定','PayPal Express Checkoutの設定', 'administrator', __FILE__, 'paypalexpresscheckout_admin_menu');
+// }
+// add_action('admin_menu', 'paypalexpresscheckout_add_admin_menu');
+//
+// // 管理画面の設定
+// function paypalexpresscheckout_admin_menu() {
+// 	echo <<<EOD
+// 	<h2>PayPal Express Checkoutの設定画面</h2>
+// 		<form action="" method="">
+// 			color:
+// 			<input type="radio" name="color" value="gold" checked> gold
+//       <input type="radio" name="color" value="blue"> blue
+// 			<input type="radio" name="color" value="silver"> silver
+// 			<input type="radio" name="color" value="black"> black
+// 			<input type="submit" value="保存">
+// 		<form>
+// EOD;
+// }
