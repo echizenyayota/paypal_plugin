574ddee8e73ea1210963b7528d61e420dfcc836d
echizenyayota <virtualmisso30@gmail.com>
2018-05-14 18:25:35 +0900

    管理画面の遷移先変更（失敗）

--- wp-content/plugins/paypal_expresscheckout/express_admin.php
+++ wp-content/plugins/paypal_expresscheckout/express_admin.php
@@ -3,7 +3,7 @@
 function paypalexpresscheckout_admin_menu() {
     echo <<<EOD
     <h2>PayPal Express Checkoutの設定画面</h2>
-        <form action="" method="">
+        <form action="paypal_expresscheckout/express.php" method="">
             color:
             <input type="radio" name="color" value="gold" checked> gold
             <input type="radio" name="color" value="blue"> blue
