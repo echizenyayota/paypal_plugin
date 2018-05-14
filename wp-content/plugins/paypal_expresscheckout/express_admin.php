5c4da2f21c136899360e36b0dcbcbdf8a9da2747
echizenyayota <virtualmisso30@gmail.com>
2018-05-14 16:01:07 +0900

    管理画面で取得した属性値(value)の確認（失敗）

--- wp-content/plugins/paypal_expresscheckout/express_admin.php
+++ wp-content/plugins/paypal_expresscheckout/express_admin.php
@@ -10,6 +10,14 @@ function paypalexpresscheckout_admin_menu() {
             <input type="radio" name="color" value="silver"> silver
             <input type="radio" name="color" value="black"> black
             <input type="submit" value="保存">
+            <input type="hidden" id="btncolor" value="">
         <form>
 EOD;
 }
+
+// クリックしたPayPalボタンの色の属性値を読み込む
+function btncolor_scripts() {
+	wp_enqueue_script( 'btncolor-script',　plugin_dir_url( __FILE__ ) . '/js/btncheckout.js', array( 'jquery' ));
+}
+
+add_action( 'wp_enqueue_scripts', 'btncolor_scripts' );
---
+++ wp-content/plugins/paypal_expresscheckout/js/buttoncheckout.js
@@ -0,0 +1,10 @@
+jQuery(function() {
+    'use strict';
+    jQuery('#btncolor').on('click', function() {
+      if (jQuery('#btncolor').val() === '') {
+        alert('Choose One!');
+      } else {
+        console.log(jQuery('form').submit());
+      }
+    });
+});
