1cc354183a8dd9d08170da99026175a729ed0b3c
echizenyayota <virtualmisso30@gmail.com>
2018-05-14 17:38:10 +0900

    wp_localize_script関数の設定とcolorプロパティを変数化

--- wp-content/plugins/paypal_expresscheckout/js/expresscheckout.js
+++ wp-content/plugins/paypal_expresscheckout/js/expresscheckout.js
@@ -1,7 +1,6 @@
 paypal.Button.render({
 
             env: 'sandbox', // sandbox | production
-
             // PayPal Client IDs - replace with your own
             // Create a PayPal app: https://developer.paypal.com/developer/applications/create
             client: {
@@ -10,7 +9,8 @@ paypal.Button.render({
             },
 
             style: {
-                color: 'blue',
+                // color: blue,
+                color: paypal_expresscheckout_param.color,
                 size: 'small'
             },
 
